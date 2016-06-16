104131e60efb6323303fe8610f16e747dbdafc10
david kilmer <dkilmer@gmail.com>
2016-06-16 19:58:37 -0400

    whatever

---
+++ CrypticMuse/ResultsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ResultsViewController.swift
+//  CrypticMuse
+//
+//  Created by david kilmer on 6/15/16.
+//  Copyright © 2016 david kilmer. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UITableViewController {
+}
--- CrypticMuse/ViewController.swift
+++ CrypticMuse/ViewController.swift
@@ -9,7 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  // MARK: Properties
+  @IBOutlet weak var searchTextField: UITextField!
+  @IBOutlet weak var anagramButton: UIButton!
+  @IBOutlet weak var hiddenButton: UIButton!
+  @IBOutlet weak var deletionButton: UIButton!
+  @IBOutlet weak var wildcardButton: UIButton!
+  @IBOutlet weak var indicatorButton: UIButton!
 
+  // MARK: Actions
+  @IBAction func anagramSearch(_ sender: AnyObject) {
+    performSegue(withIdentifier: "resultsSegue", sender: sender)
+  }
+  
+  @IBAction func hiddenSearch(_ sender: AnyObject) {
+  }
+  
+  @IBAction func deletionSearch(_ sender: AnyObject) {
+  }
+
+  @IBAction func wildcardSearch(_ sender: AnyObject) {
+  }
+  
+  @IBAction func indicatorSearch(_ sender: AnyObject) {
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
